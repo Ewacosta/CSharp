8f2923e94034ea03f7bca84a0d98a301d0634d18
Willson Sandoval <113082392+Ewacosta@users.noreply.github.com>
2023-08-07 21:12:06 -0700

    Add files via upload

---
+++ VerContenidoTxt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace VerContenidoTxt
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar la ruta del archivo texto (.txt): ");
+            string Archivo = Console.ReadLine();
+
+            if ( File.Exists(Archivo) )
+            {
+                string[] lineas = File.ReadAllLines(Archivo);
+
+                Console.WriteLine($"Contenido del archivo '{Archivo}': ");
+                foreach (string linea in lineas)
+                {
+                    Console.WriteLine(linea);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("ruta proporcionada no valida, archivo no existe.");
+            }
+        }   
+    }
+}
