f3cdf3ea0e0389ab2b32c4ac965452ea70c089eb
Willson Sandoval <113082392+Ewacosta@users.noreply.github.com>
2023-08-07 21:07:23 -0700

    Add files via upload

---
+++ ApagarPC-Windows/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ApagadoProgramado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa de apagado con temporizador");
+            Console.Write("Ingresa en cuantos minutos deseas que se apague la pc: ");
+            int minutos = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            // convertir el tiempo en segundos
+            int segundos = minutos * 60;
+
+            Console.WriteLine($"¡ATENCION! el equipo se apagara en {minutos} minutos.");
+            Console.WriteLine();
+
+            // Iniciar Programa - temporizador
+            var temporizador = new System.Threading.Timer(OffComputadora, null, segundos * 1000, System.Threading.Timeout.Infinite);
+
+            Console.WriteLine("Presiona Enter para detener el temporizador...");
+            Console.ReadLine();
+
+            // Detener Programa - temporizador
+            temporizador.Dispose();
+
+            Console.WriteLine("Temporizador detenido.");
+        
+            
+        }
+
+        static void OffComputadora(object state)
+        {
+            // Ejecutar comando para apagar la computadora
+            Process.Start("shutdown", "/s /f /t 0");
+        }
+    }
+            
+}
